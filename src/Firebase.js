419062f43a19f1783d53db817f70d2924add7e40
sssatendra <74877594+sssatendra@users.noreply.github.com>
2021-07-25 22:10:14 +0530

    changes

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,12 +2,7 @@ import firebase from 'firebase';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzIlSaLXBtxWyAxG_NoLq4YYg9sqv7QRc",
-  authDomain: "netflix-clone-da71e.firebaseapp.com",
-  projectId: "netflix-clone-da71e",
-  storageBucket: "netflix-clone-da71e.appspot.com",
-  messagingSenderId: "36362236200",
-  appId: "1:36362236200:web:14d99270aca1b64e40201d"
+ // Your firebase config here
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
